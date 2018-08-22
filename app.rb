f0a7e6aa500eb24fac6f7d8c7362c5b0d4b86f88
Andrew Wood <andrew.wood.dev@outlook.com>
2018-08-22 16:23:41 +0100

    Added form names page

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'shotgun'
 
 class Battle < Sinatra::Base
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
   end
+
+  post '/names' do
+    @p1, @p2 = params[:p1_name], params[:p2_name]
+    erb :play
+  end
+
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,6 +1,25 @@
-describe "Homepage", type: :feature do
-  it "goes to the home page" do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+# describe "Homepage", type: :feature do
+#   it "goes to the home page" do
+#     visit '/'
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature "it takes 2 names in a form" do
+  scenario "load the page" do
+    visit("/")
+    expect(page).to have_field("p1_name")
+    expect(page).to have_field("p2_name")
+  end
+end
+
+feature "it shows 2 names" do
+  scenario "load the page" do
+    visit("/")
+    fill_in "p1_name", with: "Bob"
+    fill_in "p2_name", with: "Dave"
+    click_button "Submit"
+    expect(page).to have_content("Player 1: ")
+    expect(page).to have_content("Player 2: ")
   end
 end
