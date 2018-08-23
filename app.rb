d82965bc8769b97ace28fb81cc8bf9114e711f92
Andrew Wood <andrew.wood.dev@outlook.com>
2018-08-23 15:48:38 +0100

    minor refactor

--- app.rb
+++ app.rb
@@ -22,17 +22,20 @@ class Battle < Sinatra::Base
 
   post '/p1_attacks' do
     session[:p2_hitpoints] -= 10
+    session[:message] = "P1's attack was super effective"
     redirect '/play'
   end
 
   post '/p2_attacks' do
     session[:p1_hitpoints] -= 10
+    session[:message] = "P2's attack was super effective"
     redirect '/play'
   end
 
   get '/play' do
     @p1, @p2 = session[:p1], session[:p2]
     @hp1, @hp2 = session[:p1_hitpoints], session[:p2_hitpoints]
+    @message = session[:message]
     erb :play
   end
 
